7c8500ccecca68171c00f31d5c6e589a75f1e782
Hannselthill Camacho <45092062+Hannsel101@users.noreply.github.com>
2019-02-26 16:31:09 -0800

    Add files via upload

---
+++ LoginWindow.py
@@ -0,0 +1,142 @@
+from tkinter import *
+import os
+
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+def userRegistration():
+    usernameInfo = username.get()#acquire user input for username on registration page
+    passwordInfo = password.get()#acquire user input for password on registration page
+
+    file = open(usernameInfo, "w")#open a username file with writing privilages
+    file.write(usernameInfo+"\n")#store username into file
+    file.write(passwordInfo)#store password into file
+    file.close()#close the file
+
+    usernameEntry.delete(0, END)#Clear the text from the username textbox
+    passwordEntry.delete(0, END)#Clear the text from the password textbox
+
+    Label(screen1, text = "Registration Successful!", fg = "green", font = ("Calibri", 11)).pack()
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+def register():
+    global screen1#Made global for use in userRegistration() function
+    screen1 = Toplevel(screen)#make this screen the currently used screen
+    screen1.title("Registration")#window title
+    screen1.geometry("400x300")#size of the screen
+
+    global username#globalize for use in userRegistration() function
+    global password#globalize for use in userRegistration() function
+    global usernameEntry#Made global for use in userRegistration() function
+    global passwordEntry#Made global for use in userRegistration() function
+
+    username = StringVar()#create as StringVar type
+    password = StringVar()#create as StringVar type
+
+    Label(screen1, text = "Please enter details below").pack()
+    Label(screen1, text = "").pack()
+
+    Label(screen1,text = "Username: ", justify=LEFT, width = "300", height = "2").pack()
+    Label(text = "").pack()
+    usernameEntry = Entry(screen1,textvariable = username, width = "30")#textbox for username
+    usernameEntry.pack()
+
+    Label(screen1,text = "Password: ", justify=LEFT).pack()
+    passwordEntry = Entry(screen1,textvariable = password, width = "30")#textbox for password
+    passwordEntry.pack()
+
+    Label(screen1, text = "").pack()
+
+    Button(screen1,text= "Confirm Registration", height = "2", width = "30", bg = "grey", command = userRegistration).pack()
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+def login():
+    global screen2
+    screen2 = Toplevel(screen)
+    screen2.title("Login")
+    screen2.geometry("400x300")
+    Label(screen2, text = "Please enter details below to login").pack()
+
+    global userVerification
+    global passVerification
+    global userEntry
+    global passEntry
+
+    userVerification = StringVar()
+    passVerification = StringVar()
+
+    Label(screen2, text = "Username: ").pack()
+    userEntry = Entry(screen2, textvariable = userVerification)
+    userEntry.pack()
+
+    Label(screen2, text = "Password: ").pack()
+    passEntry = Entry(screen2, textvariable = passVerification)
+    passEntry.pack()
+
+    Label(text = "").pack()
+    Button(screen2,text = "Login", width = "30", height = "2", bg = "gray", command = verifyLogin).pack()
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+def verifyLogin():
+    username1 = userEntry.get()
+    password1 = passEntry.get()
+    userEntry.delete(0,END)
+    passEntry.delete(0,END)
+
+    fileList = os.listdir(os.curdir)
+    if username1 in fileList:
+        userFile = open(username1, "r")
+        verify = userFile.read().splitlines()
+        if password1 in verify:
+            successfulLogin()
+        else:
+            failedLogin()
+    else:
+        userNotFound()
+
+def successfulLogin():
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.title("Success")
+    screen3.geometry("150x100")
+    Label(screen3,text = "Login success").pack()
+    Button(screen3,text = "OK", command = destroy3).pack()
+
+def failedLogin():
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.title("Failed Login")
+    screen3.geometry("150x100")
+    Label(screen3,text = "Login Failure").pack()
+    Button(screen3,text = "OK", command = destroy3).pack()
+
+def userNotFound():
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.title("User Error")
+    screen3.geometry("150x100")
+    Label(screen3,text = "User not found").pack()
+    Button(screen3,text = "OK", command = destroy3).pack()
+
+def destroy3():
+    screen3.destroy()
+
+#***************************************************************************************
+#mainScreen is the screen in charge of choosing register or login
+#***************************************************************************************
+def mainScreen():
+    global screen
+    screen = Tk()#main login screen
+    screen.geometry("400x300")#size of screen
+    screen.title("Login Authenticator")#window title
+    Label(text = "Ugo Chat", bg = "grey",width = "300", height="2", font =("Calibri",13)).pack()
+    Label(text = "").pack()
+    Button(text = "Login", height = "2", width = "30", command=login).pack()#login button
+    Label(text= "").pack()
+    Button(text = "Register", height = "2", width = "30", command=register).pack()#register button
+
+    screen.mainloop()#continuously check for events
+
+#----------------------------------------
+#program start
+#----------------------------------------
+mainScreen()#run mainScreen function
---
+++ testServer.py
@@ -0,0 +1,57 @@
+import socket, time, signal
+from threading import Thread
+
+#--------------------------------------------------
+#Initialize the Server
+#--------------------------------------------------
+clients = {}#stores new clients
+addresses = {}#stores new clients addresses
+BUFSIZ = 1024#Change this value to change the buffersize of sockets
+
+server = socket.socket()#server is now socket type and can receive/send through sockets
+host = '192.168.0.44'#my internal IP
+port = 50000#Port to be used for external access to my server
+server.bind((host,port))#Bind the socket type to host/port
+
+#--------------------------------------------------
+#Server Functions for listening, sending, handling, and receiving
+#--------------------------------------------------
+def portListener():#listen for new clients
+    while True:
+        client, clientAddress = server.accept()#accept the new TCP connection
+        print("%s:%s has connected." % clientAddress)#Notify server of new connection
+        client.send(bytes("Welcome to ugoChat!\r\r", "utf8")) 
+        client.send(bytes("Now type your name and press enter!","utf8"))#prompt client
+        addresses[client] = clientAddress#store clients address in list
+        Thread(target=handleClient, args=(client,)).start()#initialize new thread               
+
+def handleClient(client):#handle client interaction
+    name = client.recv(BUFSIZ).decode("utf8")#receive the clients name
+    greeting = 'Welcome %s! to exit ugoChat simply type {exit}.' % name
+    client.send(bytes(greeting, "utf8"))#instructions on how to leave
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))#Notify all other users of new client
+    clients[client] = name#Store the clients selected name
+    while True:#loop in charge of allowing the client pass messages
+        msg = client.recv(BUFSIZ)#receive message from client
+        if msg != bytes("{exit}", "utf8"):
+            broadcast(msg, name+": ")#Send message to all other users
+        else:#user wishes to disconnect
+            client.close()#remove clients socket connection
+            del clients[client]#delete client from list of clients
+            broadcast(bytes("%s has left the chat." % name, "utf8"))#broadcast exit
+            break#end of while loop
+
+def broadcast(msg, prefix=""):#handles the broadcasting of messages to all users
+    for sock in clients:#iterate through all clients receiving socket
+        sock.send(bytes(prefix, "utf8")+msg)#send the msg to each client
+#--------------------------------------------------
+#main()
+#--------------------------------------------------
+if __name__ == "__main__":
+    server.listen(5)#listen to a max of 5 connections
+    print("Awaiting new ugo...")
+    listenerThread = Thread(target=portListener)
+    listenerThread.start()
+    listenerThread.join()
+    server.close()
